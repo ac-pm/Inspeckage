2eb72eade19edc1f117b22d41c0ac098e0a183e9
ac-pm <antoniocarlosp.m@gmail.com>
2016-03-13 10:01:15 -0300

    Update Module.java

--- app/src/main/java/mobi/acpm/inspeckage/Module.java
+++ app/src/main/java/mobi/acpm/inspeckage/Module.java
@@ -3,6 +3,7 @@ package mobi.acpm.inspeckage;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.IXposedHookZygoteInit;
 import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XC_MethodReplacement;
 import de.robv.android.xposed.XSharedPreferences;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
