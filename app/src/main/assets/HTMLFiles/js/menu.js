9effaf31e74abd4990b002cdaa84b5807b1c5920
ac-pm <antoniocarlosp.m@gmail.com>
2017-02-23 12:54:31 -0300

    share popup

--- app/src/main/assets/HTMLFiles/js/menu.js
+++ app/src/main/assets/HTMLFiles/js/menu.js
@@ -130,3 +130,30 @@ var shown = true;
             shown = true;
           }
         }
+
+
+
+
+  $.fn.customerPopup = function (e, intWidth, intHeight, blnResize) {
+
+
+    e.preventDefault();
+
+
+    intWidth = intWidth || '500';
+    intHeight = intHeight || '400';
+    strResize = (blnResize ? 'yes' : 'no');
+
+
+    var strTitle = ((typeof this.attr('title') !== 'undefined') ? this.attr('title') : 'Social Share'),
+        strParam = 'width=' + intWidth + ',height=' + intHeight + ',resizable=' + strResize,
+        objWindow = window.open(this.attr('href'), strTitle, strParam).focus();
+  };
+
+
+
+  $(document).ready(function ($) {
+    $('.customer.share').on("click", function(e) {
+      $(this).customerPopup(e);
+    });
+  });
